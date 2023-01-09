2a4ebbeb56a91b62d5b7190b9d14e58ea75a1fe0
Rafael Mileris <rafaelmileris15@gmail.com>
2023-01-09 19:31:08 -0300

    Criando os scripts e códigos dos documentos

---
+++ JS_Scripts/getElementByID.js
@@ -0,0 +1,19 @@
+//Exemplo 1
+//Alterando a cor do texto
+document.getElementById("ex1").style.color = "Black"
+
+//Exemplo2
+//Alterando o estilo da fonte
+document.getElementById("ex2").style.fontFamily = "Arial Black"
+
+//Exemplo 3
+//Alterando o texto escrito
+document.getElementById("ex3").innerHTML = "Exemplo alterado"
+
+//Exemplo 4
+//Adicionando uma cor de background no texto
+document.getElementById("ex4").style.backgroundColor = "red"
+
+//Exemplo 5
+//Adicionando uma borda
+document.getElementById("ex5").style.border = "5px solid gold"

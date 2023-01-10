4e619825a26b506a8f7c7298c86d1c5879c97f30
Rafael Mileris <rafaelmileris15@gmail.com>
2023-01-10 15:19:53 -0300

    Alterações e inclusões realizadas

---
+++ JS_Scripts/getElementByClassName.js
@@ -0,0 +1,11 @@
+const colecao = document.getElementsByClassName("ex")
+
+console.log(colecao[0])
+
+console.log(colecao[1])
+
+console.log(colecao[2])
+
+console.log(colecao[3])
+
+console.log(colecao[4])
